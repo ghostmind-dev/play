76d1e301a0620abe6bed2d59bedd43248a520c08
Francois Seguin <seguin.mtl@gmail.com>
2024-05-13 23:25:26 +0000

    cleaning

--- play/install/scripts/dev.ts
+++
@@ -1,5 +0,0 @@
-import { $ } from 'npm:zx';
-
-export default async function (arg: CustomArgs, opts: CustomOptions) {
-  await $`bun build ./app/src/main.ts --outdir ./dist --target node --watch `;
-}
