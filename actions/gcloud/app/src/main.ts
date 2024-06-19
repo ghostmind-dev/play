dd438691e9bc3b8f225adccee4049716b52fc564
Francois Seguin <seguin.mtl@gmail.com>
2024-06-19 18:57:51 +0000

    updating

--- actions/gcloud/app/src/main.ts
+++ actions/gcloud/app/src/main.ts
@@ -12,10 +12,10 @@ try {
 
   $.verbose = true;
 
-  await $`apt-get update`;
-  await $`curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg`;
-  await $`echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list`;
-  await $`apt-get update && apt-get install -y google-cloud-cli`;
+  await $`sudo apt-get update`;
+  await $`curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg`;
+  await $`echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list`;
+  await $`sudo apt-get update && apt-get install -y google-cloud-cli`;
 
   if (login === 'true') {
     const GCP_SERVICE_ACCOUNT_ADMIN =
