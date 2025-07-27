08cd55da168149687b7508eab8ea596c2ab45364
Francois Seguin <admin@ghostmind.dev>
2025-07-27 15:02:32 +0000

    updating vault

--- actions/vault/app/src/main.ts
+++ actions/vault/app/src/main.ts
@@ -12,6 +12,7 @@ try {
   $.verbose = true;
 
   await $`sudo apt-get update`;
+  await $`sudo rm -f /usr/share/keyrings/hashicorp-archive-keyring.gpg`;
   await $`curl https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg`;
   await $`gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint`;
   await $`echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`;
